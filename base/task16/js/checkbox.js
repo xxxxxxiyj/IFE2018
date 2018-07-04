d55c99201e2d266dae42a666d6ff106c8fa16721
xxxxxxiyj <xiyanjun2018@163.com>
2018-07-04 22:33:32 +0800

    MIS系统（一）

--- base/task16/js/checkbox.js
+++ base/task16/js/checkbox.js
@@ -1,42 +1,31 @@
-// function 生成一组CheckBox( CheckBox容器, CheckBox选项的参数对象或者数组 ) {
-//     生成全选checkbox的html，给一个自定义属性表示为全选checkbox，比如checkbox-type="all"
-//     遍历参数对象 {
-//         生成各个子选项checkbox的html，给一个自定义属性表示为子选项
-//     }
-//     容器innerHTML = 生成好的html集合
-
-//     给容器做一个事件委托 = function() {
-//         if 是checkbox
-//             读取自定义属性
-//             if 全选
-//                 做全选对应的逻辑
-//             else
-//                 做子选项对应的逻辑
-//                 点击之前判断是否是唯一一个选中的单个checkbox，如果是则阻止这次默认点击事件
-//                 点击之后判断是否满足了全选状态，并对应修改全选checkbox的状态
-//     }
-// }
-
-function createCheckBox(parent) {
-    let child = parent.childNodes;
-    // console.log(child[0]);
-    parent.onclick = function(e) {
+/**
+ * 选择框
+ * @param  {} parent 容器
+ */
+function chooseCheckBox(parent) {
+    let checkAll  = parent.querySelector("input[checkbox-type = 'all'");  // 获取当前容器下的全选框
+    let child = parent.querySelectorAll("input[checkbox-type = 'single'"); // 获取当前容器下的选项选框
+    parent.onclick= function(e) {
         let target = e.target;
         if(target.getAttribute("type") === "checkbox") {
             let type = target.getAttribute("checkbox-type");
-            if(type === "all") {
+            if(type === "all") { // 如果点击的为全选框
+                checkAll.checked = true;
                 for(let i = 0; i < child.length; i++) {
                     child[i].checked = true;
                 }
-            } else {
-                // let num = 0; 
-                // for(let i = 0; i < child.length; i++) {
-                //     if(child[i].checked) num++;
-                // }
-                // if() {
-                //     e.preventDefault();  // 阻止此次默认点击事件
-                // }
-                
+            } else if(type == "single"){
+                let num = 0;
+                for(let i = 0; i < child.length; i++) {
+                    if(child[i].checked) num++;
+                }
+                if(num === child.length) {
+                    checkAll.checked = true;  // 若为全选，则勾选全选状态
+                }else if(num === 0){
+                    target.checked = true;    // 若其余的都为选中，则至少选中一个，不允许一个都不选
+                }else {
+                    checkAll.checked = false;
+                }
             }
         }
     }
